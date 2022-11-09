09c78415628f5c1d32a274630044517748911abc
sergio-code <115664238+sergio-code@users.noreply.github.com>
2022-11-09 18:15:05 -0300

    codigo nuevo
    incorporacion de nuevo codigo

---
+++ fork/index/index-fork.py
@@ -0,0 +1,56 @@
+#############################
+# Primer commit main App-Prueba
+# *Subir a Github
+# * Index.py
+# ejecutable.exe
+# release: 26/10/2022
+# developer: Sergio Depetris
+##############################
+import openpyxl
+#main-resources
+
+marca = "HIDROFA", "GAMMA", "KARCHER", "LUSQTOFF", "BLACK&DECKER", "NIWA", "ELECTROLUX", "DAEWO", "MOTOMEL", "ECHO", "STHILL"
+
+presion = "100", "150", "200" 
+
+#index
+marca = input('ingrese marca')
+print ('usted quiere, ' + marca)
+
+presion = input('ingrese presion')
+print ('usted quiere, ' + presion)
+
+while True:
+        marca = input('ingrese marca')
+        if marca != "NIWA":
+            print("Pone una marca valido")
+        else:
+            print("Genial")
+            break
+        continue
+
+while True:
+        presion = int(input('ingrese presion'))
+        if presion != (100):
+            print("elija una presion valido")
+        else:
+            print("Bien")
+            break
+        continue
+
+
+#variableUsuario
+
+#definimos la ubicacion del archivo
+wb = openpyxl.load_workbook("C:\Desarrollo GIT\contactos\emails\excel-emails.xlsx")
+
+print("wb.sheetname")
+
+ws = wb.get_sheet_by_name("CLIENTE")
+
+#fecha entrega
+
+"calendar"
+
+
+
---
+++ fork/resources/resource_main.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#VARIABLES PRESION\n",
+    "\n",
+    "PRESION = 1, 2, 3, 4\n",
+    "\n",
+    "#VARIABLE MARCA\n",
+    "MARCA = \"HIDROFA\", \"GAMMA\", \"KARCHER\", \"LUSQTOFF\", \"BLACK&DECKER\", \"NIWA\", \"ELECTROLUX\", \"DAEWO\", \"MOTOMEL\",\"ECHO\", \"STHILL\"\n",
+    "\n",
+    "#VARIABLE USUARIO\n",
+    "\"IMPORT\"\n",
+    "\n",
+    "#FECHAENTREGA\n",
+    "\"CALENDAR\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.11.0 64-bit",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.11.0"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "c812eb984687e98b7686c463c3fae2045e58b2a2b4e065b3dcebe3cb7d51662e"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
