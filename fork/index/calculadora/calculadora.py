5fe4066b1d3e66ff47121fdcabc5c9650e21690c
sergio-code <115664238+sergio-code@users.noreply.github.com>
2022-11-11 12:49:01 -0300

    Cambios en codigos
    Agregado de titulos

--- fork/index/calculadora/calculadora.py
+++ fork/index/calculadora/calculadora.py
@@ -5,17 +5,20 @@
 # desarrollador: Sergio Depetris
 ##############################
 
+#Realización de una secuencia de codigos para facilitar al operador realizar calculos matemáticos.
+
 number1=int(input("ingresa un numero:"))
 number2=int(input("ingresa otro numero:"))
 
 eleccion = 0
 
-while eleccion != 2:
+while eleccion != 3:
     print("""
     indique la operacion a realizar:
     
     1) suma
     2) resta
+    3) reset
         """)
 
     eleccion = int(input())
@@ -25,5 +28,7 @@ while eleccion != 2:
     if eleccion == 2:
         print(" ")
         print("resultado:", number1,"-", number2,"=",number1-number2)
+    if eleccion == 3:
+        print("Gracias por usar sergio-code")
     
     
